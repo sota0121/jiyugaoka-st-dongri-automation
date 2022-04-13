e258b32a38e7bbb3c649eabf73198229bbd62622
sota_masuda <sota.masuda@the-lionsdata.com>
2022-04-13 20:02:04 +0900

    add test for pdf exporting

--- toolbox.py
+++ toolbox.py
@@ -1,6 +1,7 @@
 import chardet
 import click
 import pandas as pd
+import pdfkit
 
 
 @click.group(name="tb", help="Toolbox cli")
@@ -29,6 +30,29 @@ def to_utf8(input: str, output: str):
     df.to_csv(output_path, encoding="utf-8", index=False)
     click.echo("Done")
 
+
+@tb.command(name="export-list", help="Export the result of matching list to pdf")
+@click.option("--input", "-i", type=str, help="Input file", required=True)
+def export_list(input: str):
+    """
+    Export the result of matching list to pdf
+    """
+    df = pd.read_csv(input)
+    df.to_html("list.html")
+    pdfkit.from_file("list.html", "list.pdf")
+    click.echo("Done")
+
+
+@tb.command(name="test")
+@click.option("--input", "-i", type=str, nargs=2, help="Input file", required=True)
+def test(input: str):
+    """
+    Test
+    """
+    click.echo(input)
+    a, b = input
+    pdfkit.from_file(["list.html", "list2.html"], "list-2.pdf")
+    click.echo("Done")
 
 
 if __name__ == "__main__":
